6dcbe4e3aa919061d97dd2dbf4ba8395df5b4f8c
Esteban Martinena Guerrero <orensbruli@gmail.com>
2018-09-21 20:14:39 +0200

    Added mouse signal to QImageWidget.

--- components/tvGames/src/modules/QImageWidget.py
+++ components/tvGames/src/modules/QImageWidget.py
@@ -1,10 +1,12 @@
 import cv2
 
-from PySide.QtCore import Qt
+from PySide.QtCore import Qt, Signal
 from PySide.QtGui import QWidget, QHBoxLayout, QLabel, QPixmap, QImage, QApplication
 
 
 class QImageWidget(QWidget):
+    mouse_pressed = Signal()
+    mouse_released = Signal()
     def __init__(self, parent=None):
         super(QImageWidget, self).__init__(parent)
         self.layout = QHBoxLayout(self)
@@ -22,7 +24,7 @@ class QImageWidget(QWidget):
         if raw_image is not None:
             if BGR:
                 raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
-            print "DEBUG: QImageWidget: %s"%str(raw_image.shape)
+            # print "DEBUG: QImageWidget: %s"%str(raw_image.shape)
             self.image = QImage(raw_image, raw_image.shape[1], \
                                 raw_image.shape[0], raw_image.shape[1] * 3,
                                 QImage.Format_RGB888)
@@ -40,3 +42,11 @@ class QImageWidget(QWidget):
             self.move(second_screen_size.left(), second_screen_size.top())
             # self.resize(second_screen_size.width(), second_screen_size.height())
             self.showMaximized()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.mouse_pressed.emit()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.mouse_released.emit()

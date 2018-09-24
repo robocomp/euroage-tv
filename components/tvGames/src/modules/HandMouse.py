94da867ac3c7c254bd39edd3644a711b80cf6404
Esteban Martinena Guerrero <orensbruli@gmail.com>
2018-09-24 20:23:53 +0200

    Added velocity filter to try to remove positions with a large distance y a short time.

--- components/tvGames/src/modules/HandMouse.py
+++ components/tvGames/src/modules/HandMouse.py
@@ -1,5 +1,10 @@
 import collections
+from datetime import datetime
+from datetime import timedelta
 
+import numpy as np
+
+start_time = datetime.now()
 
 class MultiHandMouses:
 	def __init__(self):
@@ -16,13 +21,35 @@ class MultiHandMouses:
 	def is_closed(self, id):
 		return self._mouses[id].is_closed()
 
+	def is_valid(self, id):
+		return self._mouses[id].is_valid()
+
 
 class HandMouse:
 	def __init__(self):
 		self._buffer = collections.deque(maxlen=5)
+		self._last_position = None
+		self._last_update_time = None
+		self._valid = False
 
 	def add_state(self, position, is_open):
-		self._buffer.append([position, is_open])
+		current_time = datetime.now()
+		if self._last_update_time is not None:
+			elapsed_time = self._last_update_time - current_time
+			if self._last_position is not None:
+				dist = np.linalg.norm(np.array(self._last_position) - np.array(position))
+				velocity = dist / (elapsed_time.microseconds/1000000.0)
+				print("all: ",velocity)
+				# TODO: velocity threshold should be configurable
+				if velocity < 10:
+					self._valid = True
+					print("saved: ", velocity)
+					self._buffer.append([position, is_open])
+				else:
+					self._valid = False
+		self._last_position = position
+		self._last_update_time = datetime.now()
+
 
 	def is_open(self):
 		return not self.is_closed()
@@ -34,6 +61,9 @@ class HandMouse:
 			if is_open:
 				return False
 		return True
+
+	def is_valid(self):
+		return self._valid
 
 
 if __name__ == '__main__':

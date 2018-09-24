53e35550870e917c569896b3ed277f3bd0b937c7
Esteban Martinena Guerrero <orensbruli@gmail.com>
2018-09-24 20:25:54 +0200

    Fixed a little bug with the position for the homography. Added a vale for the adjustment for the position of the seen tags. Added the apriltag_size_factor to be able to change the size of the tags on the screen.

--- components/tvGames/src/modules/CalibrationStateMachine.py
+++ components/tvGames/src/modules/CalibrationStateMachine.py
@@ -18,54 +18,65 @@ class CalibrationStateMachine:
 		self._image[:] = (255, 255, 255)
 		self._state = 0
 		self.tv_reduction = 0
+		self.apriltag_size_factor = 0.2
 		self.april_0 = cv2.imread('resources/april_0.png')
-		self.april_0 = cv2.resize(self.april_0, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+		self.april_0 = cv2.resize(self.april_0, None, fx=self.apriltag_size_factor, fy=self.apriltag_size_factor, interpolation=cv2.INTER_CUBIC)
 		self.april_1 = cv2.imread('resources/april_1.png')
-		self.april_1 = cv2.resize(self.april_1, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+		self.april_1 = cv2.resize(self.april_1, None, fx=self.apriltag_size_factor, fy=self.apriltag_size_factor, interpolation=cv2.INTER_CUBIC)
 		self.april_2 = cv2.imread('resources/april_2.png')
-		self.april_2 = cv2.resize(self.april_2, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+		self.april_2 = cv2.resize(self.april_2, None, fx=self.apriltag_size_factor, fy=self.apriltag_size_factor, interpolation=cv2.INTER_CUBIC)
 		self.april_3 = cv2.imread('resources/april_3.png')
-		self.april_3 = cv2.resize(self.april_3, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+		self.april_3 = cv2.resize(self.april_3, None, fx=self.apriltag_size_factor, fy=self.apriltag_size_factor, interpolation=cv2.INTER_CUBIC)
 		self.last_tags = []
 		self.origin_points = []
 		self.reference_points = []
 		self._homography = None
+		# TODO: It would be calculated
+		self._homography_adjustment = 30
 		self._states_values = \
 			{
 				0:
 					{
 						"tag": self.april_0,
-						"y_pos": "self.tv_reduction",
-						"x_pos": "self.tv_reduction",
+						"roi_x_pos": "self.tv_reduction",
+						"roi_y_pos": "self.tv_reduction",
 						"orig_x_pos": "5",
-						"orig_y_pos": "5"
+						"orig_y_pos": "5",
+						"ref_x_pos": "detected_tags[0].tx-self._homography_adjustment",
+						"ref_y_pos": "detected_tags[0].ty-self._homography_adjustment"
 
 					},
 				1:
 					{
 						"tag": self.april_1,
-						"y_pos": "self.screen_height - self.april_1.shape[0] - self.tv_reduction",
-						"x_pos": "self.tv_reduction",
+						"roi_x_pos": "self.tv_reduction",
+						"roi_y_pos": "self.screen_height - self.april_1.shape[0] - self.tv_reduction",
 						"orig_x_pos": "5",
-						"orig_y_pos": "self.screen_width"
+						"orig_y_pos": "self.screen_height",
+						"ref_x_pos": "detected_tags[0].tx-self._homography_adjustment",
+						"ref_y_pos": "detected_tags[0].ty+self._homography_adjustment"
 
 					},
 				2:
 					{
 						"tag": self.april_2,
-						"y_pos": "self.tv_reduction",
-						"x_pos": "self.screen_width - self.april_2.shape[1] - self.tv_reduction",
-						"orig_x_pos": "5",
-						"orig_y_pos": "self.screen_width"
+						"roi_x_pos": "self.screen_width - self.april_2.shape[1] - self.tv_reduction",
+						"roi_y_pos": "self.tv_reduction",
+						"orig_x_pos": "self.screen_width",
+						"orig_y_pos": "5",
+						"ref_x_pos": "detected_tags[0].tx+self._homography_adjustment",
+						"ref_y_pos": "detected_tags[0].ty-self._homography_adjustment"
 
 					},
 				3:
 					{
 						"tag": self.april_3,
-						"y_pos": "self.screen_height - self.april_3.shape[0] - self.tv_reduction",
-						"x_pos": "self.screen_width - self.april_3.shape[1] - self.tv_reduction",
-						"orig_x_pos": "self.screen_width",
-						"orig_y_pos": "self.screen_height"
+						"roi_x_pos": "self.screen_width - self.april_3.shape[1] - self.tv_reduction",
+						"roi_y_pos": "self.screen_height - self.april_3.shape[0] - self.tv_reduction",
+						"orig_x_pos": "self.screen_width - self.tv_reduction",
+						"orig_y_pos": "self.screen_height - self.tv_reduction",
+						"ref_x_pos": "detected_tags[0].tx+self._homography_adjustment",
+						"ref_y_pos": "detected_tags[0].ty+self._homography_adjustment"
 
 					}
 			}
@@ -76,15 +87,15 @@ class CalibrationStateMachine:
 		cal_state = self._states_values[self._state]
 		try:
 			self._image[:] = (255, 255, 255)
-			copy_roi(self._image, cal_state["tag"], eval(cal_state["y_pos"]), eval(cal_state["x_pos"]))
+			copy_roi(self._image, cal_state["tag"], eval(cal_state["roi_y_pos"]), eval(cal_state["roi_x_pos"]))
 		except:
 			print("Calibration failed")
 			self._state = -1
 		if len(detected_tags) == 1:
 			if detected_tags[0].id == self._state:
 				self.origin_points.append([eval(cal_state["orig_x_pos"]), eval(cal_state["orig_y_pos"])])
-				self.reference_points.append([detected_tags[0].tx,
-											  detected_tags[0].ty])
+				self.reference_points.append([eval(cal_state["ref_x_pos"]),
+											  eval(cal_state["ref_y_pos"])])
 				self._state = self._state + 1
 				cv2.waitKey(1000)
 				self.tv_reduction = 0

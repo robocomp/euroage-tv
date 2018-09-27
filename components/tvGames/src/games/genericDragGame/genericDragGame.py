ac86b83318be19348975d95c1c01ef1a687120a2
rcintas <rcintas@gmail.com>
2018-09-27 14:35:38 +0200

    new games

--- components/tvGames/src/games/genericDragGame/genericDragGame.py
+++ components/tvGames/src/games/genericDragGame/genericDragGame.py
@@ -4,6 +4,7 @@ import json
 import math
 import os
 import sys
+import time
 
 # Create a class for our main window
 from PyQt4.QtCore import Qt, QTimer, QPointF, pyqtSignal, QDateTime
@@ -14,16 +15,16 @@ CURRENT_PATH = os.path.dirname(__file__)
 
 
 class MyQGraphicsScene(QGraphicsScene):
-    moved = pyqtSignal(int, int, bool)
+    moved = pyqtSignal(int, int, int, bool)
 
     def mousePressEvent(self, event):
-        self.moved.emit(event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
+        self.moved.emit(-1, event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
 
     def mouseMoveEvent(self, event):
-        self.moved.emit(event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
+        self.moved.emit(-1, event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
 
     def mouseReleaseEvent(self, event):
-        self.moved.emit(event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
+        self.moved.emit(-1, event.scenePos().x(), event.scenePos().y(), event.buttons() == Qt.LeftButton)
 
 
 class DraggableItem(QGraphicsPixmapItem):
@@ -84,9 +85,10 @@ class DraggableItem(QGraphicsPixmapItem):
 
 
 class TakeDragGame(QWidget):
-    def __init__(self, width=800, height=800, parent=None):
+    def __init__(self, width=1920, height=1080, parent=None):
         super(TakeDragGame, self).__init__(parent)
         # ui
+        self.resize(width, height)
         self.width = width
         self.height = height
         self.main_layout = QHBoxLayout()
@@ -104,19 +106,16 @@ class TakeDragGame(QWidget):
         # TODO: Check if its better with opengl or not
         # self.view.setViewport(QtOpenGL.QGLWidget())
         self.main_layout.addWidget(self.view)
-#        self.setWindowState(Qt.WindowMaximized)
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_clock)
         self.clock = QGraphicsTextItem("00:00")
         self.clock.hide()
-        # self.clock.setAttribute(Qt.WA_TranslucentBackground)
         self.clock.setFont(QFont("Arial", 70, QFont.Bold))
         self.clock.setPos(self.width - self.clock.boundingRect().width(), 0)
         self.clock.setZValue(60)
         self.scene.addItem(self.clock)
         self.endMessage = QGraphicsTextItem(u"¡Has perdido!")
         self.endMessage.hide()
-        # self.endMessage.setAttribute(Qt.WA_TranslucentBackground)
         self.endMessage.setFont(QFont("Arial", 90, QFont.Bold))
         self.endMessage.setPos(self.width / 2 - self.endMessage.boundingRect().width() / 2,
                              self.height / 2 - self.endMessage.boundingRect().height() / 2)
@@ -130,10 +129,9 @@ class TakeDragGame(QWidget):
         self.grabbed = None
         self.game_config = None
         self.config = ""
-        self.init_game(os.path.join(CURRENT_PATH, 'resources/game1.json'))
-
 
     def init_game(self, config_file):
+        self.clear_scene()
         self.game_config = None
         self.grabbed = None
         self.correct_images = 0
@@ -141,13 +139,21 @@ class TakeDragGame(QWidget):
         # load config game file
         with open(os.path.join(CURRENT_PATH, config_file)) as file_path:
             self.game_config = json.load(file_path)
+        self.resize(self.game_config["size"][0], self.game_config["size"][1])
         self.create_and_add_images()
         self.draw_position(self.scene.width()/2, self.scene.height()/2, False)
         self.time = int(self.game_config["time"])
         self.endMessage.hide()
+        self.setWindowState(Qt.WindowMaximized)
         self.clock.show()
         self.update_clock()
         self.timer.start(1000)
+
+
+    def clear_scene(self):
+        if self.game_config:
+            for key, item in self.game_config["images"].items():
+                self.scene.removeItem(item["widget"])
 
 
     def end_game(self, value):
@@ -156,8 +162,11 @@ class TakeDragGame(QWidget):
             self.endMessage.setHtml(u"<font color='green'>¡Has ganado!</font>")
         else:
             self.endMessage.setHtml(u"<font color='red'>¡Has perdido!</font>")
-        self.endMessage.show()
         self.clock.hide()
+        self.endMessage.show()
+#        self.scene.update()
+#        time.sleep(3)
+        self.clear_scene()
 
 
     def update_clock(self):
@@ -168,7 +177,7 @@ class TakeDragGame(QWidget):
         self.time = self.time - 1
 
 
-    def update_pointer(self, xpos, ypos, grab):
+    def update_pointer(self,id, xpos, ypos, grab):
         self.draw_position(xpos, ypos, grab)
         if grab:
             if self.grabbed:
@@ -193,9 +202,7 @@ class TakeDragGame(QWidget):
                 xdistance = (self.grabbed.scenePos().x() + self.grabbed.width / 2) - self.grabbed.final_posex
                 ydistance = (self.grabbed.scenePos().y() + self.grabbed.height / 2) - self.grabbed.final_posey
                 distance = math.sqrt(pow(xdistance, 2) + pow(ydistance, 2))
-                print distance
                 if distance < int(self.game_config["difficult"]):
-                    print "Correct position"
                     new_xpos = self.grabbed.final_posex - self.grabbed.width / 2
                     new_ypos = self.grabbed.final_posey - self.grabbed.height / 2
                     self.grabbed.setPos(new_xpos, new_ypos)
@@ -211,7 +218,6 @@ class TakeDragGame(QWidget):
                         self.correct_images = self.correct_images - 1
                     self.grabbed.set_overlay(False)
                 self.grabbed = None
-
 
     def draw_position(self, xpos, ypos, grab):
         if grab:
@@ -251,27 +257,32 @@ class TakeDragGame(QWidget):
         xfactor = float(event.size().width()) / float(event.oldSize().width())
         yfactor = float(event.size().height()) / float(event.oldSize().height())
         for key, item in self.game_config["images"].items():
-            # update size
-            new_xsize = item["widget"].width * xfactor
-            new_ysize = item["widget"].height * yfactor
-            item["widget"].resize(new_xsize, new_ysize)
-            # update position
-            new_xpos = float(item["widget"].scenePos().x()) * xfactor
-            new_ypos = float(item["widget"].scenePos().y()) * yfactor
-            item["widget"].setPos(new_xpos, new_ypos)
-            # update final pose
-            if item["widget"].draggable:
-                new_finalx = item["widget"].final_posex * xfactor
-                new_finaly = item["widget"].final_posey * yfactor
-                item["widget"].set_final_pose(new_finalx, new_finaly)
+#TODO: resize called before widget creation
+            if "widget" in item:
+                # update size
+                new_xsize = item["widget"].width * xfactor
+                new_ysize = item["widget"].height * yfactor
+                item["widget"].resize(new_xsize, new_ysize)
+                # update position
+                new_xpos = float(item["widget"].scenePos().x()) * xfactor
+                new_ypos = float(item["widget"].scenePos().y()) * yfactor
+                item["widget"].setPos(new_xpos, new_ypos)
+                # update final pose
+                if item["widget"].draggable:
+                    new_finalx = item["widget"].final_posex * xfactor
+                    new_finaly = item["widget"].final_posey * yfactor
+                    item["widget"].set_final_pose(new_finalx, new_finaly)
 
 
 def main():
     # Again, this is boilerplate, it's going to be the same on
     # almost every app you write
     app = QApplication(sys.argv)
-    window = TakeDragGame()
+    window = TakeDragGame(1920,1080)
     window.show()
+
+    window.init_game(os.path.join(CURRENT_PATH, 'resources/game4.json'))
+
 
     # It's exec_ because exec is a reserved word in Python
     sys.exit(app.exec_())

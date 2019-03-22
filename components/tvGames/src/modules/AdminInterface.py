85eb716b8b97c953022dfeac53f43125d9de93ab
aracelivegamagro <aracelivegamagro@hotmail.com>
2019-03-22 13:14:48 +0100

    modifications in login. New python file metrics has been added to save the data of the players and the data obtained in the game

--- components/tvGames/src/modules/AdminInterface.py
+++ components/tvGames/src/modules/AdminInterface.py
@@ -8,25 +8,49 @@
 # WARNING! All changes made in this file will be lost!
 import sys
 
-from PySide2.QtCore import Signal
-from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtCore import Signal, QFile
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout
 from ui_AdminInterface import Ui_AdminInterface
 
-class AdminInterface(QMainWindow, Ui_AdminInterface):
-    close_main_window = speak = Signal(str)
-    def __init__(self):
-        super(AdminInterface, self).__init__()
-        self.setupUi(self)
+## Comprobar si así va bien
+class AdminInterface(QWidget): # crea widget vacio
+    def __init__(self,parent = None):
+        super(AdminInterface, self).__init__(parent)
+        self.mylayout = QVBoxLayout()
+        self.setLayout(self.mylayout)
+        loader = QUiLoader()
+        loader.registerCustomWidget(AdminInterface)
+        file = QFile("/home/robocomp/robocomp/components/euroage-tv/components/tvGames/src/AdminInterface_new.ui")
+        file.open(QFile.ReadOnly)
+        self.ui = loader.load(file, self.parent())
+        self.mylayout.addWidget(self.ui)
+        self.mylayout.setContentsMargins(0,0,0,0)
+        file.close()
 
     def update_admin_image(self, image):
-        self.admin_image.set_opencv_image(image, False)
+        self.ui.admin_image.set_opencv_image(image, False)
 
     def closeEvent(self, event):
         self.close_main_window.emit()
         super(AdminInterface, self).closeEvent(event)
 
+# class AdminInterface(QMainWindow, Ui_AdminInterface):
+#     close_main_window = speak = Signal(str)
+#     def __init__(self):
+#         super(AdminInterface, self).__init__()
+#         self.setupUi(self)
+#
+#     def update_admin_image(self, image):
+#         self.admin_image.set_opencv_image(image, False)
+#
+#     def closeEvent(self, event):
+#         self.close_main_window.emit()
+#         super(AdminInterface, self).closeEvent(event)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # interf = AdminInterface()
     interf = AdminInterface()
     interf.show()
     app.exec_()

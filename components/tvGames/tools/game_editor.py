f4b1488b638ac9b76770c4fde9b55cec1f0f25e1
Unknown <aracelivegamagro@hotmail.com>
2019-08-22 11:41:54 +0200

    Changes in game_editor.py
    A json file is created with the data inserted in the qt window

--- components/tvGames/tools/game_editor.py
+++ components/tvGames/tools/game_editor.py
@@ -1,12 +1,13 @@
 import signal
 import sys
-
+from collections import OrderedDict
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QWidget, QVBoxLayout, QApplication
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QApplication, QMessageBox
+import json
 
 
-class PieceWindow(QWidget):  # crea widget vacio
+class PieceWindow(QWidget):
     def __init__(self, parent=None):
         super(PieceWindow, self).__init__(parent)
         self.mylayout = QVBoxLayout()
@@ -41,7 +42,7 @@ class EditorWindow(QWidget):  # crea widget vacio
         self.ui.add_piece_button.clicked.connect(self.open_piece_window)
         self.ui.delete_piece_button.clicked.connect(self.delete_piece)
         self.ui.pieces_listWidget.itemDoubleClicked.connect(self.show_piece)
-
+        self.ui.generate_button.clicked.connect(self.generate_dict)
 
     def open_piece_window(self):
         print("Creating piece")
@@ -54,22 +55,28 @@ class EditorWindow(QWidget):  # crea widget vacio
         self.current_piece.ui.video_path_lineEdit.clear()
         self.current_piece.show()
 
-    def show_piece(self,item):
+    def show_piece(self, item):
         item_name = item.text()
         self.pieces[item_name].show()
 
     def save_piece(self):
         key = self.current_piece.ui.name_lineEdit.text()
         if key != "":
-            print ("Saving piece...")
-            print ("Name", key)
-            print ("Position", self.current_piece.ui.index_sb.value())
-            print ("Image Path", self.current_piece.ui.image_path_lineEdit.text())
-            print ("Video Path", self.current_piece.ui.video_path_lineEdit.text())
+            if key not in self.pieces.keys():
+                print ("Saving piece...")
+                print ("Name", key)
+                print ("Position", self.current_piece.ui.index_sb.value())
+                print ("Image Path", self.current_piece.ui.image_path_lineEdit.text())
+                print ("Video Path", self.current_piece.ui.video_path_lineEdit.text())
 
-            self.pieces[key] = self.current_piece
-            self.ui.pieces_listWidget.addItem(key)
-            self.current_piece.hide()
+                self.pieces[key] = self.current_piece
+                self.ui.pieces_listWidget.addItem(key)
+                self.current_piece.hide()
+
+            else:
+                QMessageBox().information(self.focusWidget(), 'Error',
+                                          'El nombre de la pieza ya existe',
+                                          QMessageBox.Ok)
 
     def delete_piece(self):
         key = self.ui.pieces_listWidget.currentItem().text()
@@ -78,13 +85,50 @@ class EditorWindow(QWidget):  # crea widget vacio
         self.ui.pieces_listWidget.takeItem(item_to_delete)
         print(len(self.pieces))
 
+    def generate_dict(self):
+        game_dict = OrderedDict()
+
+        game_dict["title"] = self.ui.title_lineEdit.text()
+        game_dict["size"] = [self.ui.game_width_sb.value(), self.ui.game_height_sb.value()]
+        game_dict["difficult"] = self.ui.difficulty_sb.value()
+        game_dict["time"] = self.ui.time_sb.value()
+        game_dict["depth"] = {"destination": "0", "piece": "10", "mouse": "50"}
+
+        piece_size = [self.ui.piece_width_sb.value(), self.ui.piece_height_sb.value()]
+        pieces_dict = {}
+
+        for name in self.pieces:
+            piece = self.pieces[name]
+            index = piece.ui.index_sb.value()
+            action_name = "action_" + str(index)
+
+            attributes_dict = OrderedDict()
+            attributes_dict["index"] = index
+            attributes_dict["title"] = name
+            attributes_dict["size"] = piece_size
+
+            attributes_dict["initial_pose"] = None
+            attributes_dict["final_pose"] = None
+
+            attributes_dict["image_path"] = piece.ui.image_path_lineEdit.text()
+            attributes_dict["video_path"] = piece.ui.video_path_lineEdit.text()
+            attributes_dict["name"] = action_name
+            attributes_dict["category"] = "piece"
+
+            pieces_dict[action_name] = attributes_dict
+
+        game_dict["images"] = pieces_dict
+
+        with open('game.json', 'w') as file:
+            json.dump(game_dict, file, indent=4)
+            print("Saved json file")
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     editor = EditorWindow()
     editor.show()
-
-    print(editor.ui.title_lineEdit.text())
 
     app.exec_()

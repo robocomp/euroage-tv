bc82c05fee2a08164e9d1cfa477ced8bb74ac043
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-03-02 13:07:40 +0100

    Added nuew widget for the duration of the game

---
+++ components/gameManager/src/widgets/CustomQTimeEdit.py
@@ -0,0 +1,65 @@
+class CustomQTimeEdit(QWidget):
+    def __init__(self, parent=None):
+        super(CustomQTimeEdit, self).__init__(parent)
+
+        self.__seconds = 0
+
+        self.__main_layout = QVBoxLayout()
+        self.setLayout(self.__main_layout)
+
+        self.__down_arrows_layout = QHBoxLayout()
+        self.__up_arrows_layout = QHBoxLayout()
+
+        f = QFont("Arial", 50, QFont.Bold)
+        self.__time_label = QLabel("00:00", self)
+        self.__time_label.setFont(f)
+
+        self.__minutes_up_button = QPushButton()
+        self.__minutes_up_button.setAutoRepeat(True)
+        self.__minutes_up_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowUp))
+        self.__seconds_up_button = QPushButton()
+        self.__seconds_up_button.setAutoRepeat(True)
+        self.__seconds_up_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowUp))
+        self.__up_arrows_layout.addWidget(self.__minutes_up_button)
+        self.__up_arrows_layout.addWidget(self.__seconds_up_button)
+
+        self.__minutes_down_button = QPushButton()
+        self.__minutes_down_button.setAutoRepeat(True)
+        self.__minutes_down_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowDown))
+        self.__seconds_down_button = QPushButton()
+        self.__seconds_down_button.setAutoRepeat(True)
+        self.__seconds_down_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowDown))
+        self.__down_arrows_layout.addWidget(self.__minutes_down_button)
+        self.__down_arrows_layout.addWidget(self.__seconds_down_button)
+
+        self.__main_layout.addLayout(self.__up_arrows_layout)
+        self.__main_layout.addWidget(self.__time_label)
+        self.__main_layout.addLayout(self.__down_arrows_layout)
+
+        self.__minutes_up_button.pressed.connect(self.up_minutes)
+        self.__minutes_down_button.pressed.connect(self.down_minutes)
+        self.__seconds_up_button.pressed.connect(self.up_seconds)
+        self.__seconds_down_button.pressed.connect(self.down_seconds)
+
+    def up_minutes(self):
+        self.__seconds += 60
+        self.update_label()
+
+    def down_minutes(self):
+        self.__seconds -= 60
+        self.update_label()
+
+    def up_seconds(self):
+        self.__seconds += 1
+        self.update_label()
+
+    def down_seconds(self):
+        self.__seconds -= 1
+        self.update_label()
+
+    def update_label(self):
+        if self.__seconds < 0:
+            self.__seconds = 0
+        minutes = int(self.__seconds / 60)
+        seconds = int(self.__seconds%60)
+        self.__time_label.setText("%02d:%02d"%(minutes, seconds))
